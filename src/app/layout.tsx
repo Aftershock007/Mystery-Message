9e7e75772859a33394488ac8db99cb025dc3d50c
Aftershock007 <ab.19u10103@btech.nitdgp.ac.in>
2024-05-01 12:54:25 +0530

    fifth commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css"
 import AuthProvider from "../context/AuthProvider"
 import { Toaster } from "@/components/ui/toaster"
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -23,6 +24,8 @@ export default async function RootLayout({ children }: RootLayoutProps) {
         <body className={inter.className}>
           {children}
           <Toaster />
+          <SpeedInsights />
+          <Analytics />
         </body>
       </AuthProvider>
     </html>
