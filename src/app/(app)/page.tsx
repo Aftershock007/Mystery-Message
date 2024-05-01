c9035bfa7b30a029e9f31c21c2e6866b5f4443aa
Aftershock007 <ab.19u10103@btech.nitdgp.ac.in>
2024-05-01 13:25:23 +0530

    sixth commit

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -16,7 +16,8 @@ export default function HomePage() {
       <main className="flex-grow flex flex-col items-center justify-center px-4 md:px-24 py-12 bg-gray-800 text-white">
         <section className="text-center mb-8 md:mb-12">
           <h1 className="text-3xl md:text-5xl font-bold">
-            Dive into the World of Anonymous Feedback
+            Dive into the World of{" "}
+            <span className="text-blue-600"> Mystery Message </span>
           </h1>
           <p className="mt-3 md:mt-4 text-base md:text-lg">
             Mystery Message - Where your identity remains a secret.
